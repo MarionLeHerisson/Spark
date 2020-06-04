79712106152dcbba6642610bfe65d4c1b90a721f
MarionLeHerisson <marion.hurteau1@gmail.com>
2020-06-04 18:28:43 +0200

    add total match number stat

--- src/Statistics.py
+++ src/Statistics.py
@@ -1,6 +1,7 @@
 import pyspark.sql.functions as F
-from pyspark.sql.types import BooleanType
 from pyspark.sql import SparkSession
+from pyspark.sql.types import BooleanType
+from pyspark.sql.window import Window
 
 # nouvelle colonne de type boolean indiquant pour chaque ligne si le match
 # a ete joue a domicile (true) ou pas (false)
@@ -9,8 +10,7 @@ def is_dom(match):
 
 def addDomCol(df):
     is_dom_udf = F.udf(is_dom, BooleanType())
-    df_with_dom = df.withColumn('match_domicile', is_dom_udf(df.match))
-    return df_with_dom
+    return df.withColumn('match_domicile', is_dom_udf(df.match))
 
 # Pour chaque adversaire de la France, calculez les statistiques suivantes :
 # - nombre de point moyen marque par la France par match
@@ -20,9 +20,13 @@ def addDomCol(df):
 # - nombre de match joue en coupe du monde
 # - plus grand nombre de penalite recu par la france dans un match
 # - nombre de penalite total recu par la France moins nombre de penalite total recu par l adversaire
+def calcStats(df):
+    totalMatchs = F.count("*").over(Window.partitionBy(df.adversaire))
+    df_adversaires = df.select(df.adversaire)#.groupBy(df.adversaire).show()
+    df_stats = df_adversaires.withColumn("match_total", totalMatchs)
 
 # ecrire le resultat dans un fichier parquet nomme stats.parquet
 
 def showStats(df):
     df_with_dom = addDomCol(df)
-    df_with_dom.show()
+    calcStats(df_with_dom)
