8f500489cb4188b4fa08ce62b36e94f511de8b66
MarionLeHerisson <marion.hurteau1@gmail.com>
2020-06-05 16:19:11 +0200

    updated join and write

--- src/JoinData.py
+++ src/JoinData.py
@@ -9,6 +9,25 @@ class JoinData():
         df_stats = spark.read.parquet('stats.parquet')
         return df_stats
 
-    def joinData(self, df):
+    def joinDfAndFile(self, df_original, df_stats):
+        df_stats = df_stats.withColumnRenamed('adversaire', 'adversaireToRemove')
+
+        df_join = df_original.join(
+            df_stats,
+            df_original.adversaire == df_stats.adversaireToRemove,
+            'left'
+        )
+
+        df_join = df_join.drop('adversaireToRemove')
+
+        return df_join
+
+    def writeFile(self, df_join):
+        df_join.write.partitionBy("date").mode('overwrite').parquet('result.parquet')
+
+    def joinData(self, df_original):
         df_stats = self.readFile()
-        df_stats.show()
+        df_join = self.joinDfAndFile(df_original, df_stats)
+        self.writeFile(df_join)
+
+        df_join.show()
--- test/CleanDataTest.py
+++ test/CleanDataTest.py
@@ -1,6 +1,5 @@
 import unittest
+from pyspark.sql import SparkSession
 
 class CleanDataTest(unittest.TestCase):
     SparkSession = SparkSession.builder().master("local[*]").getOrCreate()
-
-}
