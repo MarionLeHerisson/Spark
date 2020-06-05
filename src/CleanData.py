f2f075bcd17915e543958f125afce08c3a6e638c
MarionLeHerisson <marion.hurteau1@gmail.com>
2020-06-05 15:07:00 +0200

    removed comments

--- src/CleanData.py
+++ src/CleanData.py
@@ -2,35 +2,20 @@ from pyspark.sql import SparkSession
 
 class CleanData():
 
-    # Lire le fichier csv dans une dataframe
     def readCsv(self):
         file = "df_matches.csv"
         spark = SparkSession.builder.getOrCreate()
         return spark.read.csv(file, header=True, sep=",").cache()
 
-    # Renommer les colonnes :
-    # - X4 : match
-    # - X6 : competition
     def renameColumns(self, df):
         return df.withColumnRenamed("X4", "match").withColumnRenamed("X6", "competition")
 
-    # Selectionner les colonnes :
-    # - match
-    # - competition
-    # - adversaire
-    # - score_france
-    # - score_adversaire
-    # - penalty_france
-    # - penalty_adversaire
-    # - date
     def selectColumns(self, df):
         return df.select(df.match, df.competition, df.adversaire, df.score_france.cast('int'), df.score_adversaire.cast('int'), df.penalty_france.cast('int'), df.penalty_adversaire.cast('int'), df.date)
 
-    # Completer les valeurs nulles dans les colonnes penalty par des 0
     def fillWithZeros(self, df):
         return df.na.fill(0)
 
-    # Filtrer et garder uniquement les matchs datant de mars 1980 a aujourd hui
     def keepOnlyFromEightees(self, df):
         return df.filter(df.date >= '1980-03-01')
 
--- src/Statistics.py
+++ src/Statistics.py
@@ -4,23 +4,13 @@ from pyspark.sql.types import BooleanType
 
 class Statistics():
 
-    # nouvelle colonne de type boolean indiquant pour chaque ligne si le match
-    # a ete joue a domicile (true) ou pas (false)
-    def is_dom(self, match):
+    def isDom(self, match):
         return match[0:6] == "France"
 
     def addDomCol(self, df):
-        is_dom_udf = F.udf(self.is_dom, BooleanType())
+        is_dom_udf = F.udf(self.isDom, BooleanType())
         return df.withColumn('match_domicile', is_dom_udf(df.match))
 
-    # Pour chaque adversaire de la France, calculez les statistiques suivantes :
-    # + nombre de point moyen marque par la France par match
-    # + nombre de point moyen marque par l adversaire par match
-    # + nombre de match joue total
-    # + pourcentage de match joue a domicile pour la France
-    # - nombre de match joue en coupe du monde
-    # + plus grand nombre de penalite recu par la france dans un match
-    # + nombre de penalite total recu par la France moins nombre de penalite total recu par l adversaire
     def calcStats(self, df):
 
         df_stats = (df
@@ -38,7 +28,6 @@ class Statistics():
 
         return df_stats
 
-    # ecrire le resultat dans un fichier parquet nomme stats.parquet
     def writeStats(self, df):
         df.write.mode('overwrite').parquet('stats.parquet')
 
